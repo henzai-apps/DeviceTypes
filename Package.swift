fc4c3b4ae3b55b4965f7b121d826f3dd32325a16
noppe <noppelabs@gmail.com>
2021-06-30 19:08:19 +0900

    devices

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DeviceTypes",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(
             name: "DeviceTypes",
